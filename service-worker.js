e9311a3c18791ec2632755c16528ab60928c982f
rafarazor <rafael-santos-lima@hotmail.com>
2025-06-28 07:23:46 -0300

    Produção Bureau

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'producao-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Instala e salva os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Intercepta requisições e serve do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
